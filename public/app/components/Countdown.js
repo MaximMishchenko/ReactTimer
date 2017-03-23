8eeda8f1881e8d17ed2fdcf1f2701a125e3081e4
MaximMishchenko <maxim.mishenko@yandex.ru>
2017-03-23 23:55:23 +0200

    Added countdown functional

--- public/app/components/Countdown.js
+++ public/app/components/Countdown.js
@@ -7,12 +7,35 @@ class Countdown extends React.Component {
     super(props)
 
     this.state = {
-      count: 0
+      count: 0,
+      countdownStatus: 'stopped'
     }
   }
 
+  componentDidUpdate(prevProps, prevState){
+    if(this.state.countdownStatus !== prevState.countdownStatus){
+      switch (this.state.countdownStatus){
+        case 'started':
+          this.startTimer()
+          break;
+      }
+    }
+  }
+
+  startTimer(){
+    this.timer = setInterval(() => {
+      let newCount = this.state.count - 1
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      })
+    }, 1000)
+  }
+
   handleSetCountdown(seconds){
-    this.setState({count: seconds})
+    this.setState({
+      count: seconds,
+      countdownStatus: 'started'
+    })
   }
 
   render(){
